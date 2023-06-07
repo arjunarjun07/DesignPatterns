ffd0efc4f93c45566d115a675f3553e4d8b7bf5c
Arjun N <arjun199522@gmail.com>
2023-06-07 18:47:37 +0530

    cmnt added

--- DPProject/DPProject/Source/Creational/AbstractFactory.cpp
+++ DPProject/DPProject/Source/Creational/AbstractFactory.cpp
@@ -19,3 +19,12 @@ ITable* VintageFurnitureFactory::createTable()
 {
     return new VintageTable();
 }
+
+/*
+Uses:
+    - Use the Abstract Factory when your code needs to work with various families of related products, but you don’t want it to depend on the concrete classes of those products—they might
+      be unknown beforehand or you simply want to allow for future extensibility.
+
+    - Consider implementing the Abstract Factory when you have a class with a set of Factory Methods
+      that blur its primary responsibility.
+*/
